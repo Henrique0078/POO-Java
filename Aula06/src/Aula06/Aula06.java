418cbd808c02d42f536734b155ed8679f2a7e581
Henrique Fontenele <henriquefontenele078@gmail.com>
2023-05-12 22:56:56 -0300

    Aula06 Guanabara

---
+++ Aula06/src/Aula06/Aula06.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula06;
+
+/**
+ *
+ * @author Henrique
+ */
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+        controle.ligar();
+        controle.maisVolume();
+        controle.play();
+        controle.abrirMenu();
+        controle.fecharMenu();
+    }
+}
